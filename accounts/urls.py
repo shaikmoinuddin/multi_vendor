13fe0bf2c515920aaeb3da46e4b3b61df3cf4da5
Moin <shaikmoin0303@gmail.com>
2023-11-10 17:25:44 +0530

    login & logout feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,15 @@ urlpatterns = [
     # register user and vendor
     path('registerUser/', views.registerUser, name='registerUser'),
     path('registerVendor/', views.registerVendor, name = 'registerVendor'),
+
+    # login and logout
+    path('login/', views.login, name = 'login'),
+    path('logout/', views.logout, name = 'logout'),
+    #path('dashboard/', views.dashboard, name = 'dashboard'),
+
+    # myAccount and redirecting to their respective dashboards
+    path('myAccount/', views.myAccount, name ='myAccount'),
+    path('custDashboard/', views.custDashboard, name = 'custDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name = 'vendorDashboard'),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render,redirect
 from .forms import UserForm
 from .models import User,UserProfile
-from django.contrib import messages
+from django.contrib import messages,auth
 from vendor.forms import VendorForm
+from .utils import detectUser
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.core.exceptions import PermissionDenied
 
 
 # Create your views here.
 
+
+# restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+    
+
+# restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+
 # customer registration
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in")
+        return redirect('dashboard')
+    elif request.method == 'POST':
         #print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
@@ -52,7 +74,10 @@ def registerUser(request):
 
 # vendor registration
 def registerVendor(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in")
+        return redirect('dashboard')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         v_form = VendorForm(request.POST, request.FILES)
         if form.is_valid() and v_form.is_valid():
@@ -92,3 +117,55 @@ def registerVendor(request):
         'v_form': v_form,
     }
     return render(request, 'accounts/registerVendor.html', context)
+
+
+# login feature
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in")
+        return redirect('myAccount')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in")
+            return redirect('myAccount')
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+
+# logout feature
+def logout(request):
+    auth.logout(request)
+    messages.info(request, "You have been logged out")
+    return redirect('login')
+
+
+# myAccount
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    return render(request, 'accounts/custDashboard.html')
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    # With this vendor objects are available only for vendorDashboard.html only
+    # vendor = Vendor.objects.get(user=request.user)
+    # context = {
+    #     'vendor': vendor,
+    # }
+    return render(request, 'accounts/vendorDashboard.html')#context)
+
