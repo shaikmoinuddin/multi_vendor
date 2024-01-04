c8e2d5a708103f9938b1d63a1e01a0b63b5e96a3
Moin <shaikmoin0303@gmail.com>
2024-01-04 21:37:55 +0530

    customer dashboard & detail page setup

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from .utils import send_verification_email
 from django.contrib.auth.tokens import default_token_generator
 from vendor.models import Vendor
 from django.template.defaultfilters import slugify
+from orders.models import Order
 
 
 # Create your views here.
@@ -194,7 +195,14 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request, 'accounts/custDashboard.html')
+    orders = Order.objects.filter(user=request.user, is_ordered=True)
+    recent_orders = orders[:5]
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders': recent_orders,
+    }
+    return render(request, 'accounts/custDashboard.html', context)
 
 
 @login_required(login_url='login')
--- customers/urls.py
+++ customers/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
 
     # customer profile
     path('profile/', views.cprofile, name="cprofile"),
+
+    # customers orders
+    path('my_orders/', views.my_orders, name="customer_my_orders"),
+
+    # order_detail
+    path('order_detail/<int:order_number>/', views.order_detail, name='order_detail'),
 ]
